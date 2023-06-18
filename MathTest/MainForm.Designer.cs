e446cf3f0f4b0f79aa65fd266eb8d9384855c162
日瀬 健人 <kentnewbie@hotmail.com>
2023-06-18 11:50:40 +0800

    T

--- MathTest/MainForm.Designer.cs
+++ MathTest/MainForm.Designer.cs
@@ -28,21 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            pnlMain = new Panel();
+            txtPointP = new TextBox();
+            lblPointP = new Label();
+            btnRefresh = new Button();
+            txtPointC = new TextBox();
+            txtPointB = new TextBox();
+            txtPointA = new TextBox();
+            lblPointC = new Label();
+            lblPointB = new Label();
+            lblPointA = new Label();
+            erpMain = new ErrorProvider(components);
+            pnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)erpMain).BeginInit();
             SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            pnlMain.BackColor = SystemColors.ControlDark;
+            pnlMain.Controls.Add(txtPointP);
+            pnlMain.Controls.Add(lblPointP);
+            pnlMain.Controls.Add(btnRefresh);
+            pnlMain.Controls.Add(txtPointC);
+            pnlMain.Controls.Add(txtPointB);
+            pnlMain.Controls.Add(txtPointA);
+            pnlMain.Controls.Add(lblPointC);
+            pnlMain.Controls.Add(lblPointB);
+            pnlMain.Controls.Add(lblPointA);
+            pnlMain.Location = new Point(12, 12);
+            pnlMain.Name = "pnlMain";
+            pnlMain.Size = new Size(268, 166);
+            pnlMain.TabIndex = 0;
+            // 
+            // txtPointP
+            // 
+            txtPointP.Location = new Point(90, 98);
+            txtPointP.Name = "txtPointP";
+            txtPointP.Size = new Size(112, 27);
+            txtPointP.TabIndex = 7;
+            txtPointP.Text = "100, 100";
+            txtPointP.Validated += txtPoint_Validated;
+            // 
+            // lblPointP
+            // 
+            lblPointP.AutoSize = true;
+            lblPointP.Location = new Point(15, 101);
+            lblPointP.Name = "lblPointP";
+            lblPointP.Size = new Size(54, 19);
+            lblPointP.TabIndex = 6;
+            lblPointP.Text = "PointP";
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.Location = new Point(90, 131);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Size = new Size(69, 27);
+            btnRefresh.TabIndex = 1;
+            btnRefresh.Text = "Draw";
+            btnRefresh.UseVisualStyleBackColor = true;
+            btnRefresh.Click += btnRefresh_Click;
+            // 
+            // txtPointC
+            // 
+            txtPointC.Location = new Point(90, 69);
+            txtPointC.Name = "txtPointC";
+            txtPointC.Size = new Size(112, 27);
+            txtPointC.TabIndex = 5;
+            txtPointC.Text = "200, 150";
+            txtPointC.TextChanged += txtPoint_Validated;
+            // 
+            // txtPointB
+            // 
+            txtPointB.Location = new Point(90, 40);
+            txtPointB.Name = "txtPointB";
+            txtPointB.Size = new Size(112, 27);
+            txtPointB.TabIndex = 4;
+            txtPointB.Text = "80, 200";
+            txtPointB.TextChanged += txtPoint_Validated;
+            // 
+            // txtPointA
+            // 
+            txtPointA.Location = new Point(90, 10);
+            txtPointA.Name = "txtPointA";
+            txtPointA.Size = new Size(112, 27);
+            txtPointA.TabIndex = 1;
+            txtPointA.Text = "20, 30";
+            txtPointA.Validated += txtPoint_Validated;
+            // 
+            // lblPointC
+            // 
+            lblPointC.AutoSize = true;
+            lblPointC.Location = new Point(15, 72);
+            lblPointC.Name = "lblPointC";
+            lblPointC.Size = new Size(55, 19);
+            lblPointC.TabIndex = 3;
+            lblPointC.Text = "PointC";
+            // 
+            // lblPointB
+            // 
+            lblPointB.AutoSize = true;
+            lblPointB.Location = new Point(15, 43);
+            lblPointB.Name = "lblPointB";
+            lblPointB.Size = new Size(54, 19);
+            lblPointB.TabIndex = 2;
+            lblPointB.Text = "PointB";
+            // 
+            // lblPointA
+            // 
+            lblPointA.AutoSize = true;
+            lblPointA.Location = new Point(15, 13);
+            lblPointA.Name = "lblPointA";
+            lblPointA.Size = new Size(55, 19);
+            lblPointA.TabIndex = 1;
+            lblPointA.Text = "PointA";
+            // 
+            // erpMain
+            // 
+            erpMain.ContainerControl = this;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(979, 543);
+            Controls.Add(pnlMain);
             Name = "MainForm";
             Text = "Form1";
             Load += MainForm_Load;
             Paint += MainForm_Paint;
             Resize += MainForm_Resize;
+            pnlMain.ResumeLayout(false);
+            pnlMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)erpMain).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel pnlMain;
+        private Label lblPointC;
+        private Label lblPointB;
+        private Label lblPointA;
+        private TextBox txtPointA;
+        private TextBox txtPointC;
+        private TextBox txtPointB;
+        private Button btnRefresh;
+        private ErrorProvider erpMain;
+        private TextBox txtPointP;
+        private Label lblPointP;
     }
 }
--- MathTest/MainForm.cs
+++ MathTest/MainForm.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace MathTest
 {
     public partial class MainForm : Form
@@ -9,35 +11,71 @@ namespace MathTest
 
         public Pen GridPen;
         public Pen GridBoldPen;
+        public Pen P1Pen;
+        public Pen P2Pen;
+        public Pen P3Pen;
+        public Pen PPPen;
+        public Pen LinePen;
 
         public MainForm()
         {
             InitializeComponent();
-
+            GridPen = new Pen(Color.Black, 1);
+            GridBoldPen = new Pen(Color.Black, 2);
+            lblPointA.ForeColor = Color.Red;
+            lblPointB.ForeColor = Color.Blue;
+            lblPointC.ForeColor = Color.Green;
+            lblPointP.ForeColor = Color.White;
+            P1Pen = new Pen(lblPointA.ForeColor, 1);
+            P2Pen = new Pen(lblPointB.ForeColor, 1);
+            P3Pen = new Pen(lblPointC.ForeColor, 1);
+            PPPen = new Pen(lblPointP.ForeColor, 1);
+            LinePen = new Pen(Color.DarkGray, 2);
         }
 
 
-        public void GetGrid()
+        public void DrawGrids(Graphics g)
         {
+            for (int i = 1; i <= MaxX / 2; i += GridInterval)
+            {
+                g.DrawLine(GridPen, Origin.X + i, 0, Origin.X + i, MaxY);
+                g.DrawLine(GridPen, Origin.X - i, 0, Origin.X - i, MaxY);
+            }
 
+            for (int i = 1; i <= MaxY / 2; i += GridInterval)
+            {
+                g.DrawLine(GridPen, 0, Origin.Y + i, MaxX, Origin.Y + i);
+                g.DrawLine(GridPen, 0, Origin.Y - i, MaxX, Origin.Y - i);
+            }
+            g.DrawLine(GridBoldPen, 0, Origin.Y, MaxX, Origin.Y);
+            g.DrawLine(GridBoldPen, Origin.X, 0, Origin.X, MaxY);
         }
         private void MainForm_Paint(object sender, PaintEventArgs e)
         {
-            //Grid
+            Graphics g = e.Graphics;
+            DrawGrids(g);
+            Point A, B, C, P;
+            A = GetCoordFromText(txtPointA.Text);
+            B = GetCoordFromText(txtPointB.Text);
+            C = GetCoordFromText(txtPointC.Text);
+            P = GetCoordFromText(txtPointP.Text);
+            Point dA = GetDrawPoint(A);
+            Point dB = GetDrawPoint(B);
+            Point dC = GetDrawPoint(C);
+            Point dP = GetDrawPoint(P);
+            g.FillEllipse(P1Pen, dA);
+            g.FillEllipse(P2Pen, dB);
+            g.FillEllipse(P3Pen, dC);
+            g.FillEllipse(PPPen, dP);
 
-            for (int i = 1; i <= MaxX / 2; i += GridInterval)
-            {
-                e.Graphics.DrawLine(GridPen, Origin.X + i, 0, Origin.X + i, MaxY);
-                e.Graphics.DrawLine(GridPen, Origin.X - i, 0, Origin.X - i, MaxY);
-            }
-                
-            for (int i = 1; i <= MaxY / 2; i += GridInterval)
-            {
-                e.Graphics.DrawLine(GridPen, 0, Origin.Y + i, MaxX, Origin.Y + i);
-                e.Graphics.DrawLine(GridPen, 0, Origin.Y - i, MaxX, Origin.Y - i);
-            }
-            e.Graphics.DrawLine(GridBoldPen, 0, Origin.Y, MaxX, Origin.Y);
-            e.Graphics.DrawLine(GridBoldPen, Origin.X, 0, Origin.X, MaxY);
+            g.DrawLine(LinePen, dA, dB);
+            g.DrawLine(LinePen, dA, dC);
+            g.DrawLine(LinePen, dB, dC);
+            //Label lblDescrition = new Label
+            //{
+            //    Text = $"AF"
+            //}
+            //Draw 2 Point
         }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -52,8 +90,6 @@ namespace MathTest
             Origin.Y = ClientSize.Height / 2;
             MaxX = ClientSize.Width;
             MaxY = ClientSize.Height;
-            GridPen = new Pen(Color.Black, 1);
-            GridBoldPen = new Pen(Color.Black, 2);
             Invalidate();
         }
 
@@ -61,5 +97,47 @@ namespace MathTest
         {
             Refresh();
         }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            if (!ValidateCoordText(txtPointA.Text) ||
+               !ValidateCoordText(txtPointB.Text) ||
+               !ValidateCoordText(txtPointC.Text) ||
+               !ValidateCoordText(txtPointP.Text))
+            {
+                MessageBox.Show("öŒëÀ•W");
+                return;
+            }
+
+            Refresh();
+        }
+
+        private void txtPoint_Validated(object sender, EventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (!ValidateCoordText(tb.Text))
+                erpMain.SetError(tb, "x,y");
+            else
+                erpMain.SetError(tb, "");
+        }
+
+        public bool ValidateCoordText(string text)
+            => Regex.IsMatch(text.Replace(" ", ""), "^[0-9]+,[0-9]+$");
+
+        public Point GetCoordFromText(string text)
+        {
+            string[] s = text.Split(',');
+            return new Point(int.Parse(s[0]), int.Parse(s[1]));
+        }
+
+        public Point GetDrawPoint(Point a)
+            => new Point(Origin.X + a.X, Origin.Y - a.Y);
+
+    }
+
+    public static class Extension
+    {
+        public static void FillEllipse(this Graphics g, Pen pen, Point p, int radius = 10)
+            => g.FillEllipse(new SolidBrush(pen.Color), p.X - radius / 2, p.Y - radius / 2, radius, radius);
     }
 }
